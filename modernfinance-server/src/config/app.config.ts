1767b70184900502395c8f4e3697584a965a5cde
Arthur Ehrnrooth <arthur.ehrnrooth@live.com>
2025-08-06 08:44:09 +0300

    For railway

--- modernfinance-server/src/config/app.config.ts
+++ modernfinance-server/src/config/app.config.ts
@@ -69,7 +69,7 @@ const config: AppConfig = {
   api: {
     openai: {
       apiKey: process.env['OPENAI_API_KEY'] || '',
-      model: process.env['OPENAI_MODEL'] || 'gpt-4-turbo-preview',
+      model: process.env['OPENAI_MODEL'] || 'gpt-4o',
       maxTokens: parseInt(process.env['OPENAI_MAX_TOKENS'] || '2000', 10),
       temperature: parseFloat(process.env['OPENAI_TEMPERATURE'] || '0.7'),
     },
